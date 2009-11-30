d27f0823aec90a53351f5d449b0cae7119846448
Ahmed Fathalla <afathalla@gmail.com>
2009-11-30 22:18:08 +0200

    Added Zip Code to Places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -18,7 +18,13 @@ class PlacesController < ApplicationController
   # GET /places/1.xml
   def show
     @place = Place.find(params[:id])
-    @loc=MultiGeocoder.geocode("8 Mohamed Tawfic Diab Street Maadi Cairo Egypt")
+    
+    if (!@place.zip_code.nil?)
+    @loc=MultiGeocoder.geocode(@place.address << ' '<< @place.zip_code)
+    else
+    @loc=MultiGeocoder.geocode(@place.address)
+    end
+    
     @map=GMap.new("map_div")
     @map.control_init(:large_map=>true,:map_type=>true)
     @map.center_zoom_init([@loc.lat,@loc.lng],10)
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091130062552) do
+ActiveRecord::Schema.define(:version => 20091130194103) do
 
   create_table "cities", :force => true do |t|
     t.string   "city_name"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(:version => 20091130062552) do
     t.integer  "city_id"
     t.float    "lat"
     t.float    "lng"
+    t.integer  "zip_code"
   end
 
   create_table "sections", :force => true do |t|
