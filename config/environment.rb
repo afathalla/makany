dee51c57dc8147070b0875f60f88066a912d0a2f
unknown <afathalla@gmail.com>
2009-11-30 03:40:52 +0200

    Merging themes into master from new work laptop

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,8 @@
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 #RAILS_GEM_VERSION = '2.0.2' unless defined? RAILS_GEM_VERSION # Work laptop version
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+#RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION # New Work laptop version
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
